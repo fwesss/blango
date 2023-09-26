ce5eddccba03734c3ba37963b46a17c36a8d4a82
Wes Feller <wes@commandbar.com>
2023-09-25 21:52:49 -0700

    feat: add bootstrap library

--- blango/urls.py
+++ blango/urls.py
@@ -16,6 +16,6 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
-urlpatterns = [
-    path('admin/', admin.site.urls),
-]
+import blog.views
+
+urlpatterns = [path("admin/", admin.site.urls), path("", blog.views.index)]
