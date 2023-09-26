c331878f324807c70091e375483276b3d6c1f8cb
Wes Feller <wes@commandbar.com>
2023-09-25 20:23:17 -0700

    feat(admin): add PostAdmin class and register Tag and Post models

--- blog/admin.py
+++ blog/admin.py
@@ -1,1 +1,13 @@
-# Register your models here.
+from django.contrib import admin
+
+from blog.models import Tag, Post
+
+admin.site.register(Tag)
+
+
+class PostAdmin(admin.ModelAdmin):
+    prepopulated_fields = {"slug": ("title",)}
+    list_display = ("slug", "published_at")
+
+
+admin.site.register(Post, PostAdmin)
